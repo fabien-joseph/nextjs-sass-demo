3ce29814dabe739bdb6d3c59efd191ca79156045
Fabien Joseph <fabjoseph24@gmail.com>
2021-03-17 08:40:25 +0100

    first commit

---
+++ next.config.js
@@ -0,0 +1,13 @@
+const withSass = require('@zeit/next-sass');
+
+module.exports =
+    withSass({
+        webpack(config, options) {
+            return config;
+        },
+        cssModules: true,
+        sassLoaderOptions: {},
+        typescriptLoaderOptions: {
+            transpileOnly: false,
+        },
+    });
